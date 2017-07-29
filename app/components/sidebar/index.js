7c098cc3b66a7e89ae8ea03e816462562c997371
Rex <rex_ni@outlook.com>
2017-07-29 16:59:06 +0800

    Update index.js

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import {Link} from 'react-router';
-// import {connect} from 'react-redux'
-// import {bindActionCreators} from 'redux'
-// import {Menu, Icon} from 'antd';
-// import Logo from '../Logo';
-// import Logger from '../../utils/Logger';
-// import items from 'menu.js';  // 由于webpack中的设置, 不用写完整路径
-// import globalConfig from 'config.js';
-// import './index.less';
+import {Link} from 'react-router';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {Menu, Icon} from 'antd';
+import Logo from '../Logo';
+import Logger from '../../utils/Logger';
+import items from 'menu.js';  // 由于webpack中的设置, 不用写完整路径
+import globalConfig from 'config.js';
+import './index.less';
 import {sidebarCollapseCreator} from '../../redux/Sidebar.js';
 
 import items from '../../menu.js';  
